5d4bc3276f6674f575a179b146961c0de93c6c87
Roma Lobachenko <sw1n0@yandex.ru>
2024-01-29 14:18:41 +0300

    add project

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture
+# """Здесь надо написать тесты с использованием pytest для модуля item."""
+def monitor():
+    return Item("Монитор", 5000, 5)
+def test_Item_calculate_total_price(monitor):
+
+    assert monitor.calculate_total_price() == 25000
+
+def test_Item_apply_discount(monitor):
+
+    assert monitor.apply_discount() == None
